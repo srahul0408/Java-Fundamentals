4f2745446a2d3f654598361308b02311b9c37c7c
srahul0408 <rahul.sahani_cs22@gla.ac.in>
2025-06-10 20:25:13 +0530

    initial

---
+++ question4.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class question4{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int radius = sc.nextInt();
+		double Area = 3.14 * radius * radius;
+		System.out.println(Area);
+	}
+}
